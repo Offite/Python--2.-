2b0dcd07939111b1d8a7f9612d5844e932d0cca8
Offite <144914863+Offite@users.noreply.github.com>
2023-10-04 14:03:44 +0300

    Add files via upload

---
+++ 04.10.2023.py
@@ -0,0 +1,93 @@
+# s = input()
+# print(s[2])
+# print(s[-2])
+# print(s[:5])
+# print(s[:-2])
+# print(s[::2])
+# print(s[1::2])
+# print(s[::-1])
+# print(s[-1::-2])
+# print(len(s))
+
+
+
+# print(input().count(' ') + 1)
+
+
+
+# = input()
+#print(s[(len(s) + 1) // 2:] + s[:(len(s) + 1) // 2])
+
+
+
+# s = input()
+# if s.count('f') == 1:
+  #  print(s.find('f'))
+# elif s.count('f') >= 2:
+  # print(s.find('f'), s.rfind('f'))
+
+
+
+# s = input()
+# s = s[:s.find('h')] + s[s.rfind('h') + 1:]
+# print(s)
+
+
+
+# print(input().replace('1', 'one'))
+
+
+
+# print(input().replace('@', ''))
+
+
+
+# s = input()
+# a = s[:s.find('h') + 1]
+# b = s[s.find('h') + 1:s.rfind('h')]
+# c = s[s.rfind('h'):]
+# s = a + b.replace('h', 'H') + c
+# print(s)
+
+
+
+# a = int(input())
+# b = int(input())
+# if a < b:
+   # print(a)
+# else:
+   # print(b)
+
+
+
+# x = int(input())
+# if x > 0:
+    # print(1)
+# elif x == 0:
+    # print(0)
+# else:
+    # print(-1)
+
+
+
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# if (x1 + y1 + x2 + y2) % 2 == 0:
+    # print('YES')
+# else:
+    # print('NO')
+
+
+
+# n = int(input())
+# m = int(input())
+# k = int(input())
+# if (k%n==0 or k%0==m) and k<n*m:
+    # print('yes')
+# else:
+    # print('no')
+
+
+
